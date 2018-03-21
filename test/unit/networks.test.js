c8fd2706a4059c64a6cc7c1b68cd2e15811f4ff4
Cody Peter Mello <cody.mello@joyent.com>
2018-03-21 19:09:52 +0000

    prefix bucket names and support linklocal routes in route objects

--- test/unit/networks.test.js
+++ test/unit/networks.test.js
@@ -95,6 +95,7 @@ test('Create network', function (t) {
         gateway: '10.0.2.1',
         resolvers: ['8.8.8.8', '10.0.2.2'],
         routes: {
+            '10.0.5.0/24': 'linklocal',
             '10.0.1.0/24': '10.0.2.2',
             '10.0.3.1': '10.0.2.2'
         }
